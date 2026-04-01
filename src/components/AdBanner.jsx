8147b4f9394d5f4b557275d34207f4045b56f821
Gabriel Afotey <gabrielafotey@gmail.com>
2026-04-01 14:12:13 +0000

    removed ad banner

--- src/components/AdBanner.jsx
+++ src/components/AdBanner.jsx
@@ -1,14 +1,5 @@
 function AdBanner({ size = "leaderboard" }) {
-  const dimensions =
-    size === "rectangle"
-      ? "w-[300px] h-[250px]"
-      : "w-full max-w-[728px] h-[90px]";
-
-  return (
-    <div className={`mx-auto ${dimensions} bg-[#141414] border-2 border-dashed border-gray-600 rounded-lg flex items-center justify-center text-gray-400 text-sm tracking-wide`}>
-      Advertisement
-    </div>
-  );
+  return null;
 }
 
 export default AdBanner;
